fb000903a04d7d58d2ae99244a12ecc08919f8a0
Arko Banerjee <cybertron00@gmail.com>
2023-12-07 19:16:24 +0530

    comments added

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -1,3 +1,10 @@
+'''
+We need to do following:
+1. init a prev and curr pointer
+2. in the loop 1st re-map the next pointer to prev(swapping)
+3. in the loop 2nd shift the present pointers to next state
+'''
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,12 +12,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head):
-        prev = None    # The new null end
-        curr = head
+        prev = None     
+        curr = head 
         while curr:
             next = curr.next
             curr.next = prev
             prev = curr
             curr = next
         return prev
-            
+ 
