083ffbe374d3e8fcb59cc6d69ffff97b03e2559c
Arko Banerjee <cybertron00@gmail.com>
2023-12-07 19:18:22 +0530

    NA

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -1,8 +1,8 @@
 '''
 We need to do following:
 1. init a prev and curr pointer
-2. in the loop 1st re-map the next pointer to prev(swapping)
-3. in the loop 2nd shift the present pointers to next state
+2. in the loop re-map the next pointer to prev(swapping) line 18,19
+3. in the loop shift the present pointers to next state  line 20,21
 '''
 
 # Definition for singly-linked list.
