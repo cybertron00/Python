9c5deacdc8d3bd9c995b7515e2dcf1650adcaa14
Arko Banerjee <cybertron00@gmail.com>
2023-12-09 07:10:20 +0530

    na

--- LinkedList/middle_linked_list.py
+++ LinkedList/middle_linked_list.py
@@ -3,6 +3,16 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
+In this we have a fast and slow pointer
+the fast pointer traverses twice the distance as
+slow pointer, so when the fast pointer is at the end 
+the slow pointer will be exactly at the middle
+'''
 class Solution:
     def middleNode(self, head):
-        
+        temp = head
+        while temp and temp.next:
+            head = head.next
+            temp = temp.next.next
+        return head
